bd1d7f599bc167184a4e4bba6943ce082dfea60e
Demitriuss777 <demitriusss777@gmail.com>
2023-11-21 21:28:56 +0200

    lesson1

---
+++ lessons/lesson1/main.js
@@ -0,0 +1,11 @@
+console.log('dfFAFDAFA');
+let n = 123;
+let n1;
+n1 = 'hello';
+console.log(n, n1);
+let x = 123333;
+console.log(typeof x);
+console.log(15 % 2);
+let ns='100500fdgdsg'
+let namber =parseInt(ns)
+console.log(namber)
